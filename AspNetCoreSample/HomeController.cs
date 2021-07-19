0116579167451adadbd86ea69e6e8a3451b9ca30
Erik <erik.arevalo@gmail.com>
2021-07-19 13:21:40 -0500

    Small change index page

---
+++ AspNetCoreSample/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreSample
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            string[] days = new string []{ "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" };
+            ViewBag.Days = days;
+            return View();
+        }
+    }
+}
